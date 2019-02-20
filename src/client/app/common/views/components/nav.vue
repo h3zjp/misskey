44798538c4a0df362313f2510aaf38dea781c444
h3zjp <h3zjp@users.noreply.github.com>
2019-02-20 19:46:30 +0900

    変更

--- src/client/app/common/views/components/nav.vue
+++ src/client/app/common/views/components/nav.vue
@@ -2,8 +2,6 @@
 <span class="mk-nav">
 	<a :href="aboutUrl" target="_blank">{{ $t('about') }}</a>
 	<i>・</i>
-	<a href="/stats" target="_blank">{{ $t('stats') }}</a>
-	<i>・</i>
 	<a :href="repositoryUrl" target="_blank">{{ $t('repository') }}</a>
 	<i>・</i>
 	<a :href="feedbackUrl" target="_blank">{{ $t('feedback') }}</a>
