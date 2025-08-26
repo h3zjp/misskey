95200792df4f1ae43c3690199fb05acbca9354af
h3zjp <uso800@h3z.jp>
2025-08-27 06:57:52 +0900

    Add Analytics File

---
+++ packages/backend/src/server/web/analytics.js
@@ -0,0 +1,11 @@
+  var _paq = window._paq = window._paq || [];
+  _paq.push(["disableCookies"]);
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="https://log-ana.h3z.jp/";
+    _paq.push(['setTrackerUrl', u+'a.php']);
+    _paq.push(['setSiteId', '5']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.async=true; g.src=u+'a.js'; s.parentNode.insertBefore(g,s);
+  })();
